2b960e0a17a8c70e20719b97b47221661a4ef725
Yves Wong <weistking@hotmail.com>
2022-11-30 20:48:38 +0800

    [tc] remove Jit and add static compile script.

--- tc.cpp
+++ tc.cpp
@@ -25,48 +25,18 @@
 #include "mlir/Transforms/Passes.h"
 
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
+#include "llvm/Bitcode/BitcodeWriter.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ErrorOr.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace tc;
-
-int runJit(mlir::ModuleOp module) {
-  // Initialize LLVM targets.
-  llvm::InitializeNativeTarget();
-  llvm::InitializeNativeTargetAsmPrinter();
-
-  // Register the translation from MLIR to LLVM IR, which must happen before we
-  // can JIT-compile.
-  mlir::registerLLVMDialectTranslation(*module->getContext());
-
-  // An optimization pipeline to use within the execution engine.
-  // TODO: change enableOpt to false.
-  auto optPipeline = mlir::makeOptimizingTransformer(
-      /*optLevel=*/false ? 3 : 0, /*sizeLevel=*/0,
-      /*targetMachine=*/nullptr);
-
-  // Create an MLIR execution engine. The execution engine eagerly JIT-compiles
-  // the module.
-  mlir::ExecutionEngineOptions engineOptions;
-  engineOptions.transformer = optPipeline;
-  auto maybeEngine = mlir::ExecutionEngine::create(module, engineOptions);
-  assert(maybeEngine && "failed to construct an execution engine");
-  auto &engine = maybeEngine.get();
-
-  // Invoke the JIT-compiled function.
-  auto invocationResult = engine->invokePacked("main");
-  if (invocationResult) {
-    llvm::errs() << "JIT invocation failed\n";
-    return -1;
-  }
-
-  return 0;
-}
 
 int dumpLLVMIR(mlir::ModuleOp module) {
   // Register the translation to LLVM IR with the MLIR context.
@@ -94,7 +64,15 @@ int dumpLLVMIR(mlir::ModuleOp module) {
     llvm::errs() << "Failed to optimize LLVM IR " << err << "\n";
     return -1;
   }
+
+  // TypeInfer: llvm::Module
   llvm::errs() << *llvmModule << "\n";
+
+  // dump to .bc file for static link.
+  std::error_code EC;
+  llvm::raw_fd_ostream OS("module.llvmir", EC, llvm::sys::fs::OF_None);
+  llvm::WriteBitcodeToFile(*llvmModule, OS);
+  OS.flush();
   return 0;
 }
 
@@ -166,9 +144,9 @@ int main(void){
       dumpLLVMIR(*module);
     }
 
-    auto jitRes = runJit(*module);
-    std::cout << "JIT finished! jitRes=[" << jitRes << "]" << std::endl;
-    return jitRes;
+    // auto jitRes = runJit(*module);
+    // std::cout << "JIT finished! jitRes=[" << jitRes << "]" << std::endl;
+    return 0;
   }
   return res;
 }

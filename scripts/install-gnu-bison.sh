81aa66e93fce4d1b353b6247f50d1665a595d521
Yves Wong <weistking@hotmail.com>
2022-12-26 16:39:45 +0800

    [github-action] add compile scripts.

---
+++ scripts/install-gnu-bison.sh
@@ -0,0 +1,8 @@
+mkdir bison && cd bison
+curl https://ftp.gnu.org/gnu/bison/bison-3.8.2.tar.xz -o bison.tar.xz
+tar -xvJf bison.tar.xz
+rm bison.tar.xz
+cd bison-3.8.2
+./configure
+
+make install -j$(nproc)
---
+++ scripts/llvm-compile.sh
@@ -0,0 +1,11 @@
+git submodule update --init --depth 1
+mkdir 3rdparty/llvm-project/build && cd 3rdparty/llvm-project/build
+cmake -G Ninja ../llvm \
+   -DLLVM_ENABLE_PROJECTS=mlir \
+   -DLLVM_BUILD_EXAMPLES=OFF \
+   -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DLLVM_ENABLE_ASSERTIONS=ON \
+   -DLLVM_ENABLE_RTTI=true
+
+ninja
